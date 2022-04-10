36dab4a7b192304ccd1862bb41db7eaf4d49f35c
OwLu <OwLu@OwLus-MacBook-Air.local>
2022-04-10 14:38:18 +0800

    fixed Image component size

--- components/meetups/MeetupDetails.js
+++ components/meetups/MeetupDetails.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 const MeetupDetails = (props) => {
   return (
     <section className={classes.detail}>
-      <Image src={props.image} alt={props.title} />
+      <Image src={props.image} alt={props.title} width={500} height={500} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
