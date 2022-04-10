56c4599d509ba7e491ad10667772899243407fbc
OwLu <OwLu@OwLus-MacBook-Air.local>
2022-04-10 14:46:16 +0800

    return img tag

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -14,7 +14,7 @@ function MeetupItem(props) {
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <Image src={props.image} alt={props.title} width={500} height={500} />
+          <img src={props.image} alt={props.title} />
         </div>
         <div className={classes.content}>
           <h3>{props.title}</h3>
